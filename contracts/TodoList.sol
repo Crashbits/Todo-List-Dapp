a68e5f70459154ac4691ae752205fc1772a95d55
Humberto Ruiz <chube_24@hotmail.com>
2021-12-05 20:52:26 -0700

    add client-side ui

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -2,4 +2,21 @@ pragma solidity ^0.5.0;
 
 contract TodoList {
     uint256 public taskCount = 0;
+
+    struct Task {
+        uint256 id;
+        string description;
+        bool completed;
+    }
+
+    mapping(uint256 => Task) public tasks;
+
+    function addTask(string memory _content) public {
+        taskCount++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
+
+    constructor() public {
+        addTask("First task");
+    }
 }
